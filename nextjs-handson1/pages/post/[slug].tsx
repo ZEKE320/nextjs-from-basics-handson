df6d95770301040492021bcd66ee56be4f55189a
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-09-12 21:07:40 +0900

    ✨ slugを用いてPostの詳細をJSON形式で表示する機能を追加

---
+++ nextjs-handson1/pages/post/[slug].tsx
@@ -0,0 +1,69 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { Post } from "../../types/Post";
+import { getPostContents, getPosts } from "../index";
+
+type StaticProps = {
+  post?: Post;
+};
+
+type StaticPathsParams = {
+  slug: string;
+};
+
+export const getStaticPaths: GetStaticPaths<StaticPathsParams> = async () => {
+  const posts = await getPosts();
+  const paths: {
+    params: { slug: string };
+  }[] = [];
+
+  posts.forEach((post) => {
+    const slug = post.slug;
+    if (slug) {
+      paths.push({
+        params: { slug },
+      });
+    }
+  });
+  return { paths, fallback: "blocking" };
+};
+
+export const getStaticProps: GetStaticProps<
+  StaticProps,
+  StaticPathsParams
+> = async ({ params, preview }) => {
+  const notFoundProps = {
+    props: {},
+    redirect: {
+      destination: "/404",
+    },
+  };
+
+  if (!params) {
+    return notFoundProps;
+  }
+
+  const { slug } = params;
+  const posts = await getPosts(slug);
+  const post = posts.shift();
+  if (!post) {
+    return notFoundProps;
+  }
+
+  const contents = await getPostContents(post);
+  post.contents = contents;
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+const PostPage: NextPage<StaticProps> = ({ post }) => {
+  if (!post) {
+    return null;
+  }
+
+  return <div>{JSON.stringify(post)}</div>;
+};
+
+export default PostPage;
