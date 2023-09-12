1f53c94309513179e5799d0c28f28791722fd38d
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-09-12 12:05:31 +0900

    🔥 生成された_document.tsxの内容を削除

--- nextjs-handson1/pages/_document.tsx
+++ nextjs-handson1/pages/_document.tsx
@@ -1,13 +1,1 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+import { Html, Head, Main, NextScript } from "next/document";
