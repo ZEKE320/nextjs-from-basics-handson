a41b95be9ad52333fb2cfb2a5db98ecd9011e85e
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-09-12 16:58:10 +0900

    ✨ Homeコンポーネントの実装

--- nextjs-handson1/pages/index.tsx
+++ nextjs-handson1/pages/index.tsx
@@ -1,7 +1,10 @@
 import { Client } from "@notionhq/client";
-import { GetStaticProps } from "next";
+import dayjs from "dayjs";
+import { GetStaticProps, NextPage } from "next";
+import prism from "prismjs";
+import { useEffect } from "react";
+import styles from "../styles/Home.module.css";
 import { Content, Post } from "../types/api/Post";
-import { NextPage } from "next";
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -128,8 +131,71 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
 };
 
 const Home: NextPage<StaticProps> = ({ post }) => {
-  console.log(post);
-  return <div></div>;
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  if (!post) {
+    return null;
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.post}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.timestampWrapper}>
+          <div>
+            <div className={styles.timestamp}>
+              作成日時: {dayjs(post.createdTs).format("YYYY-MM-DD HH:mm:ss")}
+            </div>
+            <div className={styles.timestamp}>
+              更新日時: {dayjs(post.lastEditedTs).format("YYYY-MM-DD HH:mm:ss")}
+            </div>
+          </div>
+        </div>
+        <div>
+          {post.contents.map((content, index) => {
+            const key = `${post.id}_${index}`;
+            switch (content.type) {
+              case "heading_2":
+                return (
+                  <h2 key={key} className={styles.heading2}>
+                    {content.text}
+                  </h2>
+                );
+              case "heading_3":
+                return (
+                  <h3 key={key} className={styles.heading3}>
+                    {content.text}
+                  </h3>
+                );
+              case "paragraph":
+                return (
+                  <p key={key} className={styles.paragraph}>
+                    {content.text}
+                  </p>
+                );
+              case "code":
+                return (
+                  <pre
+                    key={key}
+                    className={`${styles.code} lang-${content.language}`}
+                  >
+                    <code>{content.text}</code>
+                  </pre>
+                );
+              case "quote":
+                return (
+                  <blockquote key={key} className={styles.quote}>
+                    {content.text}
+                  </blockquote>
+                );
+            }
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
