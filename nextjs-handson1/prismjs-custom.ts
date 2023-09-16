913702fe83d43a1901a53485fe35fcb1d05d19da
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-09-16 03:48:05 +0000

    🔧 Prism.jsの設定ファイルを追加

---
+++ nextjs-handson1/prismjs-custom.ts
@@ -0,0 +1,20 @@
+import Prism from "prismjs";
+
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-graphql";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-markdown";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-yaml";
+import "prismjs/components/prism-rust";
+import "prismjs/components/prism-java";
+
+export default Prism;
