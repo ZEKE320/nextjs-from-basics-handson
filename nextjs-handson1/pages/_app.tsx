286ca0f612581eca762f19f8861288e9b36ef7ed
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-09-16 03:50:28 +0000

    ⚡️ フォントの読み込みをNext.jsの機能で最適化

--- nextjs-handson1/pages/_app.tsx
+++ nextjs-handson1/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.scss";
+import { Noto_Sans_JP } from "next/font/google";
 import type { AppProps } from "next/app";
 
+const notoSansJP = Noto_Sans_JP({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={notoSansJP.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- nextjs-handson1/pages/_document.tsx
+++ nextjs-handson1/pages/_document.tsx
@@ -3,16 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Index = () => {
   return (
     <Html>
-      <Head>
-        <link
-          href={
-            "https://fonts.googleapis.com/css2" +
-            "?family=Noto+Sans+JP:wght@400;500;700" +
-            "&display=swap"
-          }
-          rel="stylesheet"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
