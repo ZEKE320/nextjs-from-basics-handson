000bce54f9ec0a247286df3db2c73c8676ef848d
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-09-16 09:04:03 +0000

    🎨 コードフォーマットの修正

--- nextjs-handson2/tailwind.config.ts
+++ nextjs-handson2/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -8,5 +8,5 @@ const config: Config = {
   ],
   theme: {},
   plugins: [],
-}
-export default config
+};
+export default config;
